2aa324f54560a665fadb8a3b14ca9081661d0644
alex chousmith <alex@ninthlink.com>
2013-05-08 22:16:50 +0000

    mobile metatags

--- sites/all/themes/qcbc/template.php
+++ sites/all/themes/qcbc/template.php
@@ -1,3 +1,44 @@
 <?php
 
 /* hmm... */
+
+/**
+* Preprocesses the wrapping HTML.
+*
+* @param array &$variables
+*   Template variables.
+*/
+function qcbc_preprocess_html(&$vars) {
+// <meta name="HandheldFriendly" content="True" />
+    $metatag = array(
+        '#type' => 'html_tag',
+        '#tag' => 'meta',
+        '#attributes' => array(
+            'name' =>  'HandheldFriendly',
+            'content' => 'True',
+        )
+    );
+    drupal_add_html_head($metatag, 'meta_handheldfriendly');
+
+// <meta name="MobileOptimized" content="1366" />
+    $metatag = array(
+        '#type' => 'html_tag',
+        '#tag' => 'meta',
+        '#attributes' => array(
+            'name' =>  'MobileOptimized',
+            'content' => '1920',
+        )
+    );
+    drupal_add_html_head($metatag, 'meta_mobileoptimized');
+    
+// <meta name="viewport" content="width=1366,maximum-scale=1.0" />
+    $metatag = array(
+        '#type' => 'html_tag',
+        '#tag' => 'meta',
+        '#attributes' => array(
+            'name' =>  'viewport',
+            'content' => 'width=1920,maximum-scale=1.0',
+        )
+    );
+    drupal_add_html_head($metatag, 'meta_viewport');
+}
