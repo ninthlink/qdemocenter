8dd414042c05e672d1034681fc7e47f6792d52f0
alex chousmith <alex@ninthlink.com>
2013-05-08 20:42:56 +0000

    highlight active row of which demo we're on

---
+++ sites/all/themes/qcbc/templates/views-view-unformatted--nodequeue-demos.tpl.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div<?php if ($classes_array[$id]) {
+	  if ( ( arg(0) == 'node' ) && ( arg(1) == $view->result[$id]->nid ) ) {
+		  $classes_array[$id] .= ' active';
+	  }
+	  print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php
+	
+//	  if ( $view->result[$id] ) echo '<pre style="display:none" title="'. $view->result[$id]->nid .'">'. print_r($view->result[$id],true) .'</pre>';
+	  
+	print $row;
+	
+	?>
+  </div>
+<?php endforeach; ?>
